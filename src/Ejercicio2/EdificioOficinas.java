8ddc6c86f1d2559aa6f8367c14642afb8eadde85
David Resca <davidresca9@gmail.com>
2026-04-15 17:18:07 -0300

    agrego class EdificioOficinas

---
+++ src/Ejercicio2/EdificioOficinas.java
@@ -0,0 +1,15 @@
+package Ejercicio2;
+
+public class EdificioOficinas implements IEdificio {
+	private int numeroOficinas;
+    private double superficie;
+	
+    public EdificioOficinas(double superficie, int numeroOficinas) {
+        this.superficie = superficie;
+        this.numeroOficinas = numeroOficinas;
+    }
+ 
+    
+    // implementar método de la interfaz para que deje de tirar error x)
+
+}
