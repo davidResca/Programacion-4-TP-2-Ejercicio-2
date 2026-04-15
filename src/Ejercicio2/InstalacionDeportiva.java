b4ec45f31fbcbb70d08dca1315539e93b15355fe
David Resca <davidresca9@gmail.com>
2026-04-15 15:22:54 -0300

    ADD: initial Ej2 -- Classes: Edificio, InstalacionDeportiva, mainEjercicio2

---
+++ src/Ejercicio2/InstalacionDeportiva.java
@@ -0,0 +1,7 @@
+package Ejercicio2;
+
+public interface InstalacionDeportiva {
+	
+	int getTipoInstalacion();
+
+}
---
+++ src/Ejercicio2/mainEjercicio2.java
@@ -0,0 +1,8 @@
+package Ejercicio2;
+
+public class mainEjercicio2 {
+	
+    public static void main(String[] args) {
+
+    }
+}
